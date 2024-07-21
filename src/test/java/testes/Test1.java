358576be59cfc575d5f7abad3170512d5a78c859
KanchanNikose <kanchannikose9@gmail.com>
2024-07-22 00:11:12 +0530

    This is first commit

---
+++ src/test/java/testes/Test1.java
@@ -0,0 +1,13 @@
+package testes;
+
+import org.testng.annotations.Test;
+
+public class Test1 {
+
+	@Test
+	public void Demo1()
+	{
+		System.out.println("Hi this is first-demo commit");
+	}
+
+}
---
+++ src/test/java/testes/Test2.java
@@ -0,0 +1,13 @@
+package testes;
+
+import org.testng.annotations.Test;
+
+public class Test2 {
+
+	@Test
+	public void demo2()
+	{
+		System.out.println("Hi this is demo2-commit");
+	}
+
+}
